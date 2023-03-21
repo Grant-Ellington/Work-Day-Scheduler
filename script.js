411de6b050b46c63d28975b05bd3ae5715125131
Grant-Ellington <gellingtonem6@gmail.com>
2023-03-21 14:32:27 -0500

    added clock

--- script.js
+++ script.js
@@ -20,4 +20,12 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  
+
+  function displayTime(){
+    currentTime = dayjs().format('MMM d, YYYY [at] hh:mm:ss')
+    $('#currentDay').text(currentTime)
+  }
+
+  timer = setInterval(displayTime,1000)
 });
