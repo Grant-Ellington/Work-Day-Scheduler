b1b8cc9a85cdcdd3f0089c8aab98cc242e3a16f6
Grant-Ellington <gellingtonem6@gmail.com>
2023-03-21 14:43:32 -0500

    added event listener

--- script.js
+++ script.js
@@ -9,8 +9,11 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  var saveBtn = $('.saveBtn') 
+  saveBtn.on('click', function(){
+    console.log('hit')
+  })
 
-  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
