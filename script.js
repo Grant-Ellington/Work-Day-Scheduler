9dedb3404f526be1498c1db44da39a1300b417dd
Grant-Ellington <gellingtonem6@gmail.com>
2023-03-22 17:05:40 -0500

    added save to local storage

--- script.js
+++ script.js
@@ -9,13 +9,100 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  var nineHr = $('hour-9')
-  var saveBtn = $('.saveBtn') 
-  var textarea = $('textarea')
-  saveBtn.on('click', function(){
-    savedNote = textarea.val() 
-    localStorage.setItem('savedNote', savedNote)
-  })
+  var nineHr = $('#hour-9');
+  var tenHr = $('#hour-10');
+  var elevenHr = $('#hour-11');
+  var twelveHr = $('#hour-12');
+  var thirteenHr = $('#hour-13');
+  var fourteenHr = $('#hour-14');
+  var fifteenHr = $('#hour-15');
+  var sixteenHr = $('#hour-16');
+  var seventeenHr = $('#hour-17');
+
+  var saveBtnNine = nineHr.children('button');
+  var saveBtnTen = tenHr.children('button');
+  var saveBtnEleven = elevenHr.children('button');
+  var saveBtnTwelve = twelveHr.children('button');
+  var saveBtnThirteen = thirteenHr.children('button');
+  var saveBtnFourteen = fourteenHr.children('button');
+  var saveBtnFifteen = fifteenHr.children('button');
+  var saveBtnSixteen = sixteenHr.children('button');
+  var saveBtnSeventeen = seventeenHr.children('button');
+
+  var textareaNine = nineHr.children('textarea');
+  var textareaTen = tenHr.children('textarea');
+  var textareaEleven = elevenHr.children('textarea');
+  var textareaTwelve = twelveHr.children('textarea');
+  var textareaThirteen = thirteenHr.children('textarea');
+  var textareaFourteen = fourteenHr.children('textarea');
+  var textareaFifteen = fifteenHr.children('textarea');
+  var textareaSixteen = sixteenHr.children('textarea');
+  var textareaSeventeen = seventeenHr.children('textarea')
+
+  saveBtnNine.on('click', function(){
+    savedNote = textareaNine.val() 
+    localStorage.setItem('savedNoteNine', savedNote)
+  });
+  saveBtnTen.on('click', function(){
+    savedNote = textareaTen.val() 
+    localStorage.setItem('savedNoteTen', savedNote)
+  });
+  saveBtnEleven.on('click', function(){
+    savedNote = textareaEleven.val() 
+    localStorage.setItem('savedNoteEleven', savedNote)
+  });
+  saveBtnTwelve.on('click', function(){
+    savedNote = textareaTwelve.val() 
+    localStorage.setItem('savedNoteTwelve', savedNote)
+  });
+  saveBtnThirteen.on('click', function(){
+    savedNote = textareaThirteen.val() 
+    localStorage.setItem('savedNoteThirteen', savedNote)
+  });
+  saveBtnFourteen.on('click', function(){
+    savedNote = textareaFourteen.val() 
+    localStorage.setItem('savedNoteFourteen', savedNote)
+  });
+  saveBtnFifteen.on('click', function(){
+    savedNote = textareaFifteen.val() 
+    localStorage.setItem('savedNoteFifteen', savedNote)
+  });
+  saveBtnSixteen.on('click', function(){
+    savedNote = textareaSixteen.val() 
+    localStorage.setItem('savedNoteSixteen', savedNote)
+  });
+  saveBtnSeventeen.on('click', function(){
+    savedNote = textareaSevenTeen.val() 
+    localStorage.setItem('savedNoteSeventeen', savedNote)
+  });
+  
+  perviousNoteNine = localStorage.getItem('savedNoteNine')
+  textareaNine.text(perviousNoteNine)
+
+  perviousNoteTen = localStorage.getItem('savedNoteTen')
+  textareaTen.text(perviousNoteTen);
+
+  perviousNoteEleven = localStorage.getItem('savedNoteEleven')
+  textareaEleven.text(perviousNoteEleven);
+
+  perviousNoteTwelve = localStorage.getItem('savedNoteTwelve')
+  textareaTwelve.text(perviousNoteTwelve)
+
+  perviousNoteThirteen = localStorage.getItem('savedNoteThirteen')
+  textareaThirteen.text(perviousNoteThirteen)
+
+  perviousNoteFourteen = localStorage.getItem('savedNoteFourteen')
+  textareaFourteen.text(perviousNoteFourteen)
+
+  perviousNoteFifteen = localStorage.getItem('savedNoteFifteen')
+  textareaFifteen.text(perviousNoteFifteen);
+
+  perviousNoteSixteen = localStorage.getItem('savedNoteSixteen')
+  textareaSixteen.text(perviousNoteSixteen);
+
+  perviousNoteSeventeen = localStorage.getItem('savedNoteSeventeen')
+  textareaSeventeen.text(perviousNoteSeventeen)
+  
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -28,8 +115,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
 
-  perviousNote = localStorage.getItem('savedNote')
-  textarea.text(perviousNote)
   // TODO: Add code to display the current date in the header of the page.
   
 
